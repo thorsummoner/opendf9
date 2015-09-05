017b0da99018ae20d831d1f690fa55a687049f6f
Dylan Grafmyre <thorsummoner@live.com>
2015-09-05 10:50:21 -0700

    Paint cursor

--- df9/mission_controll/eden_launcher.py
+++ df9/mission_controll/eden_launcher.py
@@ -24,38 +24,104 @@ class EdenLauncher(MissionControll):
         self.galaxy_map_bg = GdkPixbuf.Pixbuf.new_from_file(self.GALAXY_MAP_BG)
 
         # Connect drawing area to this class's handler
-        app.builder.get_object('eden_drawingarea').connect(
-            'draw',
-            self.DrawingAreaHandler(self).draw,
+        eden_drawingarea =  app.builder.get_object('eden_drawingarea')
+        eden_drawingarea.set_events(
+            Gdk.EventMask.POINTER_MOTION_MASK
+            | Gdk.EventMask.BUTTON_PRESS_MASK
+            | Gdk.EventMask.BUTTON_RELEASE_MASK
         )
+        eden_drawing_handler = self.DrawingAreaHandler(self, app)
+        eden_drawingarea.connect('draw', eden_drawing_handler.draw)
+
+        eden_drawingarea.connect('motion_notify_event', eden_drawing_handler.motion_notify_event)
+        eden_drawingarea.connect('button_release_event',  eden_drawing_handler.button_release_event)
 
     class DrawingAreaHandler(object):
-        def __init__(self, parent):
+
+        cursor_pos = None
+        DIAGONAL_RATE = 0.3
+        AMBER_ACCENT_SHIFT = 1.5
+
+        def __init__(self, parent, app):
             super(EdenLauncher.DrawingAreaHandler, self).__init__()
+
             self.parent = parent
+            self.app = app
 
         def draw(self, widget, ct):
             width = widget.get_allocated_width()
             height = widget.get_allocated_height()
 
-            # Paint the universe
-            img_width = float(self.parent.galaxy_map_bg.get_width())
-            img_height = float(self.parent.galaxy_map_bg.get_height())
-            # Scale
-            width_ratio = width / img_width
-            height_ratio = height / img_height
-            scale_xy = max(height_ratio, width_ratio)
-            # Center
-            off_x =  (width  - round(img_width*scale_xy)) //2
-            off_y =  (height - round(img_height*scale_xy)) //2
+            cursor_x = 0
+            cursor_y = 0
+            try:
+                cursor_x = self.cursor_pos.x
+                cursor_y = self.cursor_pos.y
+                # And reset
+                self.cursor_pos = None
+            except AttributeError, err:
+                pass
 
-            # Paint
-            ct.save()
 
-            ct.translate(off_x, off_y)
-            ct.scale(scale_xy, scale_xy)
+            if 'universe':
+                # Paint the universe
+                img_width = float(self.parent.galaxy_map_bg.get_width())
+                img_height = float(self.parent.galaxy_map_bg.get_height())
+                # Scale
+                width_ratio = width / img_width
+                height_ratio = height / img_height
+                scale_xy = max(height_ratio, width_ratio)
+                # Center
+                off_x =  (width  - round(img_width*scale_xy)) //2
+                off_y =  (height - round(img_height*scale_xy)) //2
 
-            Gdk.cairo_set_source_pixbuf(ct, self.parent.galaxy_map_bg, 0, 0)
-            ct.paint()
+                # Paint
+                ct.save()
 
-            ct.restore()
+                ct.translate(off_x, off_y)
+                ct.scale(scale_xy, scale_xy)
+
+                Gdk.cairo_set_source_pixbuf(ct, self.parent.galaxy_map_bg, 0, 0)
+                ct.paint()
+
+                ct.restore()
+
+            if cursor_y > 0 and cursor_x > 0:
+                # Paint a cursor, Vertical Line
+                self._amber_line(ct, cursor_x, 0, cursor_x, height)
+
+                # Paint a cursor, Diagonal Line
+                self._amber_line(
+                    ct,
+                    0, cursor_y - (cursor_x * self.DIAGONAL_RATE),
+                    width, cursor_y + ((width - cursor_x) * self.DIAGONAL_RATE)
+                )
+
+
+        def _amber_line(self, ct, x1, y1, x2, y2):
+            """
+                Stroke a two-tone line.
+            """
+
+            ct.set_source_rgb(227/256.0, 98/256.0, 59/256.0)
+            ct.set_line_width(2)
+            ct.move_to(x1+self.AMBER_ACCENT_SHIFT, y1+self.AMBER_ACCENT_SHIFT)
+            ct.line_to(x2+self.AMBER_ACCENT_SHIFT, y2+self.AMBER_ACCENT_SHIFT)
+            ct.stroke()
+
+            ct.set_source_rgb(242/256.0, 179/256.0, 69/256.0)
+            ct.set_line_width(2)
+            ct.move_to(x1, y1)
+            ct.line_to(x2, y2)
+            ct.stroke()
+
+
+        def motion_notify_event(self, widget, event):
+            """
+                Update cursor position, (event.{x,y}) and repaint view.
+            """
+            self.cursor_pos = event
+            self.app.window.get_window().invalidate_rect(None, False)
+
+        def button_release_event(self, widget, event):
+            print('click')
