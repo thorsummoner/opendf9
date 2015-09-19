39dac1584ec843f64b9e492acd9b490e74781f77
Dylan Grafmyre <thorsummoner@live.com>
2015-09-19 13:36:04 -0700

    Large refactor, approaching pylintliness.

--- df9/mission_controll/eden_launcher.py
+++ df9/mission_controll/eden_launcher.py
@@ -3,10 +3,10 @@
 """
 
 import math
+from collections import namedtuple
 
 from gi.repository import Gdk
 from gi.repository import GdkPixbuf
-import cairo
 
 from df9.mission_controll import MissionControll
 from df9.fontloader import FontLoader
@@ -24,20 +24,28 @@ PARSEC_LIGHTYEAR = 3.262
 
 ORIGINAL_COORD = (0.15116763695057492, 0.37949796350990855)
 
+Coordinate = namedtuple('Coordinate', ['x', 'y'])
+
 class EdenLauncher(MissionControll):
+    """
+        New game starting conditions screen
+    """
     CONTAINER = 'eden'
 
-    GALAXY_MAP_BG = 'df9/assets/images/author-eso/2048px-Wide_Field_Imager_view_of_a_Milky_Way_look-alike_NGC_6744.jpg'
+    GALAXY_MAP_BG = 'df9/assets/images/author-eso/' \
+        + '2048px-Wide_Field_Imager_view_of_a_Milky_Way_look-alike_NGC_6744.jpg'
     FONT_FACE_FILE = 'df9/assets/fonts/ProFontWindows.ttf'
     FONT_FACE = FontLoader().cairo_font_face_from_file(FONT_FACE_FILE)
     REGIONAL_FACTORS_FILE = 'df9/assets/eden/noise-stack.ora'
 
+    #pylint: disable=C0326
     REGIONAL_FACTORS = (
         {'name': 'Stellar Density',       'key': 'density'},
         {'name': 'Warpgate Proximity',    'key': 'warp'},
         {'name': 'Threat Factor',         'key': 'threat'},
         {'name': 'Magnetic Interference', 'key': 'interference'},
     )
+    #pylint: enable=C0326
 
     cursor = True
     last_settlement = ORIGINAL_COORD
@@ -59,7 +67,7 @@ class EdenLauncher(MissionControll):
         self.galaxy_map_bg = GdkPixbuf.Pixbuf.new_from_file(self.GALAXY_MAP_BG)
 
         # Connect drawing area to this class's handler
-        eden_drawingarea =  app.builder.get_object('eden_drawingarea')
+        eden_drawingarea = app.builder.get_object('eden_drawingarea')
         eden_drawingarea.set_events(
             Gdk.EventMask.POINTER_MOTION_MASK
             | Gdk.EventMask.BUTTON_PRESS_MASK
@@ -69,7 +77,7 @@ class EdenLauncher(MissionControll):
         eden_drawingarea.connect('draw', eden_drawing_handler.draw)
 
         eden_drawingarea.connect('motion_notify_event', eden_drawing_handler.motion_notify_event)
-        eden_drawingarea.connect('button_release_event',  eden_drawing_handler.button_release_event)
+        eden_drawingarea.connect('button_release_event', eden_drawing_handler.button_release_event)
 
         # Get data form elements
         self.eden_accept = app.builder.get_object('eden_accept')
@@ -103,10 +111,16 @@ class EdenLauncher(MissionControll):
 
     @property
     def selected_coord(self):
+        """
+            Selected cell
+        """
         return self._selected_coord
 
     @selected_coord.setter
     def selected_coord(self, value):
+        """
+            When selecting a cell, ui elements are updated
+        """
         if value:
             self._selected_coord = self.get_cell(*value)
             self.selected_settlement = value
@@ -139,9 +153,11 @@ class EdenLauncher(MissionControll):
             self.eden_distance.set_text('')
             self.eden_arrival.set_text('')
 
-
-
-    def get_cell(self, cursor_percent_x, cursor_percent_y):
+    @staticmethod
+    def get_cell(cursor_percent_x, cursor_percent_y):
+        """
+            Return grid cell for percent
+        """
         cell_x = math.ceil(cursor_percent_x * CELLS_WIDTH)
         cell_y = math.ceil(cursor_percent_y * CELLS_HEIGHT)
         return (
@@ -149,31 +165,45 @@ class EdenLauncher(MissionControll):
             int(cell_y if cell_y > 0 else 1),
         )
 
-    def get_parsec(self, cursor_percent_x, cursor_percent_y):
+    @staticmethod
+    def get_parsec(cursor_percent_x, cursor_percent_y):
+        """
+            Return distance in Parsecs from top left (0, 0) of universe
+        """
         return (
             cursor_percent_x * UNIVERSE_SIZE,
             cursor_percent_y * UNIVERSE_SIZE,
         )
 
     def grid_toggle(self, widget):
+        """
+            Toggle painted element and redraw.
+        """
         self.grid = widget.get_active()
         self.app.window.get_window().invalidate_rect(None, False)
 
     def on_eden_accept_accept(self, *_):
+        """ on_eden_accept_accept """
         self.eden_accept.set_sensitive(False)
         self.eden_launch.set_sensitive(True)
 
     def on_eden_accept_cancel(self, *_):
+        """ on_eden_accept_cancel """
         self.selected_coord = None
 
     def on_eden_launch_launch(self, *_):
+        """ on_eden_launch_launch """
         self.app.replace_mission_controll(self.app.loadingscreen)
 
     def on_eden_launch_cancel(self, *_):
+        """ on_eden_launch_cancel """
         self.selected_coord = None
         self.eden_launch.set_sensitive(False)
 
     class DrawingAreaHandler(object):
+        """
+            Handle draw events of drawingarea
+        """
 
         DIAGONAL_RATE = 0.49
         AMBER_ACCENT_SHIFT = 1
@@ -191,7 +221,7 @@ class EdenLauncher(MissionControll):
             self.parent = parent
             self.app = app
 
-        def draw(self, widget, ct):
+        def draw(self, widget, cairo_ct):
             width = widget.get_allocated_width()
             height = widget.get_allocated_height()
 
@@ -202,7 +232,7 @@ class EdenLauncher(MissionControll):
                 cursor_y = self.cursor_pos.y
                 # And reset
                 self.cursor_pos = None
-            except AttributeError, err:
+            except AttributeError:
                 pass
 
 
@@ -215,22 +245,22 @@ class EdenLauncher(MissionControll):
                 height_ratio = height / img_height
                 scale_xy = max(height_ratio, width_ratio)
                 # Center
-                off_x =  (width  - round(img_width*scale_xy)) //2
-                off_y =  (height - round(img_height*scale_xy)) //2
+                off_x = (width  - round(img_width*scale_xy)) //2
+                off_y = (height - round(img_height*scale_xy)) //2
 
                 # Paint
-                ct.save()
+                cairo_ct.save()
 
-                ct.translate(off_x, off_y)
-                ct.scale(scale_xy, scale_xy)
+                cairo_ct.translate(off_x, off_y)
+                cairo_ct.scale(scale_xy, scale_xy)
 
-                Gdk.cairo_set_source_pixbuf(ct, self.parent.galaxy_map_bg, 0, 0)
-                ct.paint()
+                Gdk.cairo_set_source_pixbuf(cairo_ct, self.parent.galaxy_map_bg, 0, 0)
+                cairo_ct.paint()
 
-                ct.restore()
+                cairo_ct.restore()
 
             if self.parent.grid:
-                ct.set_source_rgba(
+                cairo_ct.set_source_rgba(
                     self.COLOR_AMBER[0],
                     self.COLOR_AMBER[1],
                     self.COLOR_AMBER[2],
@@ -238,53 +268,55 @@ class EdenLauncher(MissionControll):
                 )
                 width_step = width/60.0
                 height_step = height/60.0
-                for x in range(60):
-                    ct.move_to(x*width_step, 0)
-                    ct.line_to(x*width_step, height)
-                for y in range(60):
-                    ct.move_to(0, y*height_step)
-                    ct.line_to(width, y*height_step)
-                ct.stroke()
+                for grid_x in range(60):
+                    cairo_ct.move_to(grid_x*width_step, 0)
+                    cairo_ct.line_to(grid_x*width_step, height)
+                for grid_y in range(60):
+                    cairo_ct.move_to(0, grid_y*height_step)
+                    cairo_ct.line_to(width, grid_y*height_step)
+                cairo_ct.stroke()
 
             if 'last_settlement':
-                self._amber_marker(ct,
+                self._amber_marker(
+                    cairo_ct,
                     self.parent.last_settlement[0]*width,
                     self.parent.last_settlement[1]*height,
                 )
             if self.parent.selected_settlement:
-                self._amber_marker(ct,
+                self._amber_marker(
+                    cairo_ct,
                     self.parent.selected_settlement[0]*width,
                     self.parent.selected_settlement[1]*height,
                 )
-                ct.set_dash([14.0, 6.0])
+                cairo_ct.set_dash([14.0, 6.0])
                 self._amber_line(
-                    ct,
+                    cairo_ct,
                     self.parent.last_settlement[0]*width,
                     self.parent.last_settlement[1]*height,
                     self.parent.selected_settlement[0]*width,
                     self.parent.selected_settlement[1]*height,
                 )
-                ct.set_dash([])
+                cairo_ct.set_dash([])
 
 
             if self.parent.cursor and cursor_y > 0 and cursor_x > 0:
                 # Paint a cursor, Vertical Line
-                self._amber_line(ct, cursor_x, 0, cursor_x, height)
+                self._amber_line(cairo_ct, cursor_x, 0, cursor_x, height)
 
                 # Paint a cursor, Diagonal Line
                 self._amber_line(
-                    ct,
+                    cairo_ct,
                     0, cursor_y - (cursor_x * self.DIAGONAL_RATE),
                     width, cursor_y + ((width - cursor_x) * self.DIAGONAL_RATE)
                 )
                 # Dashed counter-diagonal line
-                ct.set_dash([14.0, 6.0])
+                cairo_ct.set_dash([14.0, 6.0])
                 self._amber_line(
-                    ct,
+                    cairo_ct,
                     cursor_x-width, cursor_y + (width * self.DIAGONAL_RATE),
-                     cursor_x+width, cursor_y - (width * self.DIAGONAL_RATE)
+                    cursor_x+width, cursor_y - (width * self.DIAGONAL_RATE)
                 )
-                ct.set_dash([])
+                cairo_ct.set_dash([])
 
                 coord = list(self.parent.get_cell(
                     cursor_x / width,
@@ -294,20 +326,29 @@ class EdenLauncher(MissionControll):
                 coord[1] = (coord[1] if 0 < coord[1] < CELLS_HEIGHT else 0)
 
                 if 'coordinate_info_offset':
-                    ct.set_font_face(self.parent.FONT_FACE)
+                    cairo_ct.set_font_face(self.parent.FONT_FACE)
 
-                    ct.set_font_size(20)
+                    cairo_ct.set_font_size(20)
                     text_coord = "({:>2}, {:>2})".format(*coord)
                     text_coord_extents = dict(zip(
                         TEXT_EXTENTS_KEYS,
-                        ct.text_extents(text_coord)
+                        cairo_ct.text_extents(text_coord)
                     ))
 
                     text_coord_x = cursor_x - text_coord_extents['width']
                     text_coord_y = cursor_y + text_coord_extents['height']
-                    self._amber_text(ct,
-                        (text_coord_x - self.COORD_X_OFF if text_coord_x - self.COORD_X_OFF > 10 else cursor_x + self.COORD_X_OFF),
-                        (text_coord_y + self.COORD_Y_OFF if text_coord_y + self.COORD_Y_OFF < height - 10 else cursor_y - self.COORD_Y_OFF),
+                    self._amber_text(
+                        cairo_ct,
+                        (
+                            text_coord_x - self.COORD_X_OFF
+                            if text_coord_x - self.COORD_X_OFF > 10
+                            else cursor_x + self.COORD_X_OFF
+                        ),
+                        (
+                            text_coord_y + self.COORD_Y_OFF
+                            if text_coord_y + self.COORD_Y_OFF < height - 10
+                            else cursor_y - self.COORD_Y_OFF
+                        ),
                         text_coord
                     )
                 if 'regional_factors':
@@ -316,7 +357,7 @@ class EdenLauncher(MissionControll):
                     for factor in self.parent.REGIONAL_FACTORS:
                         line += line_step
                         self._amber_text(
-                            ct, 10, line,
+                            cairo_ct, 10, line,
                             '{:>21}: {:.3}'.format(
                                 factor['name'],
                                 self.parent.regional_factors_data.layers[
@@ -326,50 +367,60 @@ class EdenLauncher(MissionControll):
                         )
 
 
-        def _amber_line(self, ct, x1, y1, x2, y2):
+        def _amber_line(self, cairo_ct, x1, y1, x2, y2):
             """
                 Stroke a two-tone line.
             """
 
-            ct.set_source_rgba(1, 1, 1, 0.5)
-            ct.set_line_width(2.2)
-            ct.move_to(x1+self.AMBER_ACCENT_SHIFT/2.0, y1+self.AMBER_ACCENT_SHIFT/2.0)
-            ct.line_to(x2+self.AMBER_ACCENT_SHIFT/2.0, y2+self.AMBER_ACCENT_SHIFT/2.0)
-            ct.stroke()
+            cairo_ct.set_source_rgba(1, 1, 1, 0.5)
+            cairo_ct.set_line_width(2.2)
+            cairo_ct.move_to(x1+self.AMBER_ACCENT_SHIFT/2.0, y1+self.AMBER_ACCENT_SHIFT/2.0)
+            cairo_ct.line_to(x2+self.AMBER_ACCENT_SHIFT/2.0, y2+self.AMBER_ACCENT_SHIFT/2.0)
+            cairo_ct.stroke()
 
-            ct.set_source_rgb(*self.COLOR_DARK_AMBER)
-            ct.set_line_width(1.4)
-            ct.move_to(x1+self.AMBER_ACCENT_SHIFT, y1+self.AMBER_ACCENT_SHIFT)
-            ct.line_to(x2+self.AMBER_ACCENT_SHIFT, y2+self.AMBER_ACCENT_SHIFT)
-            ct.stroke()
+            cairo_ct.set_source_rgb(*self.COLOR_DARK_AMBER)
+            cairo_ct.set_line_width(1.4)
+            cairo_ct.move_to(x1+self.AMBER_ACCENT_SHIFT, y1+self.AMBER_ACCENT_SHIFT)
+            cairo_ct.line_to(x2+self.AMBER_ACCENT_SHIFT, y2+self.AMBER_ACCENT_SHIFT)
+            cairo_ct.stroke()
 
-            ct.set_source_rgb(*self.COLOR_AMBER)
-            ct.set_line_width(1)
-            ct.move_to(x1, y1)
-            ct.line_to(x2, y2)
-            ct.stroke()
+            cairo_ct.set_source_rgb(*self.COLOR_AMBER)
+            cairo_ct.set_line_width(1)
+            cairo_ct.move_to(x1, y1)
+            cairo_ct.line_to(x2, y2)
+            cairo_ct.stroke()
 
-        def _amber_text(self, ct, x, y, msg):
-            ct.set_source_rgb(*self.COLOR_DARK_AMBER)
-            ct.move_to(x+self.AMBER_ACCENT_SHIFT, y+self.AMBER_ACCENT_SHIFT)
-            ct.show_text(msg)
+        def _amber_text(self, cairo_ct, coord_x, coord_y, msg):
+            """
+                Sylized text painter
+            """
+            cairo_ct.set_source_rgb(*self.COLOR_DARK_AMBER)
+            cairo_ct.move_to(coord_x+self.AMBER_ACCENT_SHIFT, coord_y+self.AMBER_ACCENT_SHIFT)
+            cairo_ct.show_text(msg)
 
-            ct.set_source_rgb(*self.COLOR_AMBER)
-            ct.move_to(x, y)
-            ct.show_text(msg)
+            cairo_ct.set_source_rgb(*self.COLOR_AMBER)
+            cairo_ct.move_to(coord_x, coord_y)
+            cairo_ct.show_text(msg)
 
-        def _amber_marker(self, ct, x, y):
-            ct.set_source_rgb(*self.COLOR_DARK_AMBER)
-            ct.arc(x, y, 3, 0, 2*math.pi)
-            ct.fill()
-            ct.set_source_rgb(*self.COLOR_AMBER)
-            ct.arc(x+self.AMBER_ACCENT_SHIFT, y+self.AMBER_ACCENT_SHIFT, 3, 0, 2*math.pi)
-            ct.fill()
-            self._amber_line(ct, x, y, x-8, y-8)
-            self._amber_line(ct, x, y, x+8, y-8)
-            self._amber_line(ct, x, y, x, y-24)
+        def _amber_marker(self, cairo_ct, coord_x, coord_y):
+            """
+                Stylized coordinate marker
+            """
+            cairo_ct.set_source_rgb(*self.COLOR_DARK_AMBER)
+            cairo_ct.arc(coord_x, coord_y, 3, 0, 2*math.pi)
+            cairo_ct.fill()
+            cairo_ct.set_source_rgb(*self.COLOR_AMBER)
+            cairo_ct.arc(
+                coord_x+self.AMBER_ACCENT_SHIFT,
+                coord_y+self.AMBER_ACCENT_SHIFT,
+                3, 0, 2*math.pi
+            )
+            cairo_ct.fill()
+            self._amber_line(cairo_ct, coord_x, coord_y, coord_x-8, coord_y-8)
+            self._amber_line(cairo_ct, coord_x, coord_y, coord_x+8, coord_y-8)
+            self._amber_line(cairo_ct, coord_x, coord_y, coord_x, coord_y-24)
 
-        def motion_notify_event(self, widget, event):
+        def motion_notify_event(self, _, event):
             """
                 Update cursor position, (event.{x,y}) and repaint view.
             """
@@ -377,6 +428,9 @@ class EdenLauncher(MissionControll):
             self.app.window.get_window().invalidate_rect(None, False)
 
         def button_release_event(self, widget, event):
+            """
+                User selected point in universe
+            """
             width = widget.get_allocated_width()
             height = widget.get_allocated_height()
 
